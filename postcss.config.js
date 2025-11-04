cdaed2581422a3853f550a1855c04323836b5d5a
MatheusTeixeiraGaldino <departamentopessoal@otaviolage.com>
2025-11-04 14:58:01 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+### **📄 ARQUIVO 8:** `.gitignore`
+```
+node_modules
+dist
+.env
+.env.local
+.DS_Store
